3ea6f978498891a2145b488f7ad203988b2dff88
jesus.maneiro <jesus.maneiro@netexlearning.com>
2020-08-11 09:37:33 +0200

    feature
    add percent to Question Results

--- src/components/QuestionResults.js
+++ src/components/QuestionResults.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Box from '@material-ui/core/Box';
 
 class QuestionResults extends Component {
 
@@ -23,12 +24,22 @@ class QuestionResults extends Component {
 
   showResult = (option, totalVotes) => {
     const yourVote = this.checkIsYourVote(option);
+    const progress = option.votes.length / totalVotes * 100;
 
     return (
       <div>
         {yourVote && this.showYourVote()}
         <Typography variant="subtitle1" className="question-margin">Would you rather {option.text}?</Typography>
-        <LinearProgress variant={'determinate'} value={ option.votes.length / totalVotes * 100 }/>
+        <Box display="flex" alignItems="center">
+          <Box width="100%" mr={1}>
+            <LinearProgress variant={'determinate'} value={progress}/>
+          </Box>
+          <Box minWidth={35}>
+            <Typography variant="body2" color="textSecondary">
+              {`${Math.round(progress)}%`}
+            </Typography>
+          </Box>
+        </Box>
         <Typography variant="h6" className="question-margin">{option.votes.length} out of {totalVotes} votes</Typography>
       </div>
     );
